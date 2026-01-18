a5c5c7a1862381771b18fa2d8a141c20d1791452
diephil <diepphil@gmail.com>
2026-01-18 22:53:43 +0100

    refactor: improve tags

--- apps/nextjs/src/instrumentation.ts
+++ apps/nextjs/src/instrumentation.ts
@@ -2,13 +2,13 @@ import { registerOTel } from "@vercel/otel";
 import { OpikExporter } from "opik-vercel";
 
 export function register() {
+  const env = process.env.VERCEL_ENV || "development";
+  const commitSha = process.env.VERCEL_GIT_COMMIT_SHA || "";
+  const vercelUrl = process.env.VERCEL_URL || "";
   registerOTel({
     serviceName: "homecuistot-hackathon",
     traceExporter: new OpikExporter({
-      tags: ["nextjs", process.env.NODE_ENV],
-      metadata: {
-        env: process.env.NODE_ENV,
-      },
+      tags: ["nextjs", env, commitSha, vercelUrl].filter(Boolean),
     }),
   });
 }
