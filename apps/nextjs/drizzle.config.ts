dabe42ffdfe1b9997bec6db0527d6342fc77bf16
diephil <diepphil@gmail.com>
2026-01-26 19:44:48 +0100

    feat: phase 3

--- apps/nextjs/drizzle.config.ts
+++ apps/nextjs/drizzle.config.ts
@@ -1,16 +1,12 @@
 import { defineConfig } from 'drizzle-kit'
 
-if (!process.env.DATABASE_URL_DIRECT) {
-  throw new Error('DATABASE_URL_DIRECT is required in environment')
-}
-
 export default defineConfig({
   schema: './src/db/schema/index.ts',
   out: './src/db/migrations',
   dialect: 'postgresql',
   verbose: true,
   dbCredentials: {
-    url: process.env.DATABASE_URL_DIRECT,
+    url: process.env.DATABASE_URL_DIRECT!,
   },
   migrations: {
     table: '__drizzle_migrations',
